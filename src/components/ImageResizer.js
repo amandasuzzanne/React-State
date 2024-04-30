23cd168cde653dba63f63c236b9442ef3f1df0b9
amandasuzzanne <amandasuzanne12@gmail.com>
2024-04-30 11:12:15 +0300

    Add a div for the paragraph

--- src/components/ImageResizer.js
+++ src/components/ImageResizer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './ImageResizer.css'
 import brain from '../images/brain.jpeg'
 
@@ -6,46 +6,50 @@ function ImageResizer() {
     // State variables for width and height
     const [width, setImageWidth] = useState(100);
     const [height, setImageHeight] = useState(100);
-  
+
     // Event handler for width slider change
     const handleWidthChange = (e) => {
-      setImageWidth(parseInt(e.target.value));
+        setImageWidth(parseInt(e.target.value));
     };
-  
+
     // Event handler for height slider change
     const handleHeightChange = (e) => {
-      setImageHeight(parseInt(e.target.value));
+        setImageHeight(parseInt(e.target.value));
     };
 
-  return (
-    <div>
-        <div className='slider'>
+    return (
+        <div>
+            <div className='main'>
+                <div>
+                    <p><strong>Practice Working With State</strong></p>
+                </div>
+
+                <div className='slider'>
+                    <label>Width:</label>
+                    <input
+                        type="range"
+                        min="50"
+                        max="500"
+                        value={width}
+                        onChange={handleWidthChange}
+                    />
+
+                    <label>Height:</label>
+                    <input
+                        type="range"
+                        min="50"
+                        max="500"
+                        value={height}
+                        onChange={handleHeightChange}
+                    />
+                </div>
+            </div>
+
             <div>
-                <label>Width:</label>
-                <input
-                type="range"
-                min="50"
-                max="500"
-                value={width}
-                onChange={handleWidthChange}
-                />
-            
-                <label>Height:</label>
-                <input
-                type="range"
-                min="50"
-                max="500"
-                value={height}
-                onChange={handleHeightChange}
-                />
+                <img src={brain} alt="brain" className="resizable-image" style={{ width: `${width}px`, height: `${height}px` }} />
             </div>
         </div>
-
-      <div>
-        <img src={brain} alt="brain" className="resizable-image" style={{ width: `${width}px`, height: `${height}px` }} />
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ImageResizer
